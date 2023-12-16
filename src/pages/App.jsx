48382d63e4ece4a53771da83b4279caf989a7fa0
Eme Hache <luisalfonsodsg@outlook.com>
2023-12-16 12:27:03 -0500

    refactor: reorder folder structure

---
+++ src/pages/App.jsx
@@ -0,0 +1,26 @@
+import Testimonials from '../components/Testimonials'
+import Features from '../components/Features'
+import Details from '../components/Details'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Hero from '../components/Hero'
+
+function App() {
+  return (
+    <div className='app'>
+      <Header />
+
+      <Hero />
+
+      <Features />
+
+      <Details />
+
+      <Testimonials />
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App
